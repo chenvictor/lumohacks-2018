62324a551043dc4d57f0d0fb08f540859b9b3504
Morgan Mo <morganandfacebook19@gmail.com>
2018-09-15 23:36:55 -0700

    second commit

---
+++ Assets/MyAssets/MainCharacter/Controller/TextboxManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextboxManager : MonoBehaviour {
+
+    TextAsset textFile;
+    string[] textLines;
+
+    GameObject textbox;
+    Text textArea;
+
+	// Use this for initialization
+	void Start () {
+        textbox.SetActive(false);
+        if (textFile != null) {
+            textLines = textFile.text.Split('\n');
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

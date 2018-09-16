a4fe4ec5761fa307498f17782187350ac098789e
Morgan Mo <morganandfacebook19@gmail.com>
2018-09-16 00:40:25 -0700

    Player can walk
    player always walk

--- Assets/MyAssets/MainCharacter/Controller/PlayerController.cs
+++ Assets/MyAssets/MainCharacter/Controller/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;             //Floating point variable to store the player's movement speed.
+    public float speed = 0.01f;             //Floating point variable to store the player's movement speed.
 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
@@ -15,19 +15,25 @@ public class PlayerController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = 0;
 
         //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveVertical = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveHorizontal = 0.06f;
+        }
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
+        rb2d.MovePosition(rb2d.position + movement);
     }
 }
--- Assets/MyAssets/MainCharacter/Controller/TextboxManager.cs
+++ Assets/MyAssets/MainCharacter/Controller/TextboxManager.cs
@@ -5,22 +5,36 @@ using UnityEngine.UI;
 
 public class TextboxManager : MonoBehaviour {
 
-    TextAsset textFile;
-    string[] textLines;
+    public TextAsset textFile;
+    public string[] textLines;
 
-    GameObject textbox;
-    Text textArea;
+    public GameObject textbox;
+    public Text textArea;
+    public int curIdx;
+    private int endIdx;
 
 	// Use this for initialization
 	void Start () {
-        textbox.SetActive(false);
         if (textFile != null) {
             textLines = textFile.text.Split('\n');
         }
+        curIdx = -1;
+        endIdx = textLines.Length;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (curIdx >= 0 && curIdx < endIdx)
+        {
+            textbox.SetActive(true);
+            textArea.text = textLines[curIdx];
+        }
+        else {
+            textbox.SetActive(false);
+        }
+  
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            curIdx += 1;
+        }
 	}
 }
